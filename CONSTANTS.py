ff3416998e1d2ce23f9a2b4871cc689a9f4dc44e
Arsalan <bazzazarsalan@gmail.com>
2026-07-19 13:06:58 +0330

    Update CONSTANTS.py

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -4,6 +4,6 @@ ARCHIVE_FILE_DIRECTORY__OF_MONTHLY_REPORT = r"D:\Data Science\Projects\SIEM Repo
 ARCHIVE_FILE_DIRECTORY__OF_QUARTERLY_REPORT = r"D:\Data Science\Projects\SIEM Report Automation With Python Project\archive\successful\quarterly"
 MAPPING_FILES_DIRECTORY = r"D:\Data Science\Projects\SIEM Report Automation With Python Project\mappings"
 LOGS_DIRECTORY = r"D:\Data Science\Projects\SIEM Report Automation With Python Project\logs"
-SHEET_NAMES__AND__CORRESPONDING_MONTHLY_SUM_COORDINATES = {"ArcSightRules": list(),
-                                                           "ArcSightRulesBMI": list(),
-                                                           "ArcSightRulesMY": list()}
+SHEET_NAMES__AND__CORRESPONDING_MONTHLY_SUM_COORDINATES = {"sheetname1": list(),
+                                                           "sheetname2": list(),
+                                                           "sheetname3": list()}
